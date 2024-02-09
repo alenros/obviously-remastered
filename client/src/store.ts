cf682df363c4ce718a8af79d7fb02ae63f7ebd88
simon s <gel@gmx.com>
2024-02-09 23:02:34 +0200

    refactor (client): Remove unused methods in players store

--- client/src/store.ts
+++ client/src/store.ts
@@ -7,28 +7,7 @@ function createPlayersStore() {
     return {
         subscribe,
         set,
-        update,
-        // TODO: This should actually refresh the list for all connected players
-        refreshList: async (roomId: string) => {
-            console.log("Updating players list");
-            await fetch(`http://localhost:5000/api/v1/rooms/${roomId}/players`)
-                .then(async (res) => await res.json())
-                .then(data => {
-                    console.log(data);
-                    update(_ => data.players.map((player: Player) => {
-                        return { id: player.id, name: player.name };
-                    }))
-                })
-                .catch((err) => {
-                    console.log(err);
-                    set([]);
-                });
-        },
-        removePlayer: (playerId: number) => {
-            // update(players => {
-            //     return players.filter(player => player.id != playerId);
-            // });
-        }
+        update
     };
 }
 
